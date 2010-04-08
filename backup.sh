77540fa54fcaa3c3a7f131d62ef95896007bca42
Boris Barbulovski <bbarbulovski@gmail.com>
2010-04-08 20:27:10 +0000

    Bat files update.

--- backup.sh
+++ backup.sh
@@ -4,4 +4,4 @@ DATE=`date +%Y%m%d%H%M%S`
 
 # TODO: please set zip command not to delete empty dirs(ex: bin)
 
-zip backup-mkfusion-0.4.1_$DATE.zip * -r -x "*.dll" -x "obj/*" -x "WINDOWS/*.exe" -x "bin/*.exe" -x "*.zip"
+zip backup-mkfusion-0.4.1_$DATE.zip * -r -x "*.dll" -x "obj/*" -x "WINDOWS/*.exe" -x "bin/*.exe" -x "*.zip" -x ".svn"
