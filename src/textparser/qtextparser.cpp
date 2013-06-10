1c76b3ed53ef72e9dabc74973f99487d2d1cc3cc
Boris Barbulovski <bbarbulovski@gmail.com>
2013-06-10 20:19:54 +0000

    Add LGPL notice

--- src/textparser/qtextparser.cpp
+++ src/textparser/qtextparser.cpp
@@ -1,3 +1,25 @@
+/****************************************************************************
+**
+** Author: Boris Barbulovski(bbarbulovski@gmail.com)
+**
+** This file is part of the MKFusion(http://mkfusion.bokicsoft.com/ or
+** https://sourceforge.net/projects/mkfusion/) project.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+**
+** GNU General Public License Usage
+** This file may be used under the terms of the GNU
+** General Public License version 3.0 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU General Public License version 3.0 requirements will be
+** met: http://www.gnu.org/copyleft/gpl.html.
+**
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
 #include "qtextparser.h"
 
 #include <limits.h>
