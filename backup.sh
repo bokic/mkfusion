d3d33d65e55c70b948ab6e41421f25c2f460c40c
Boris Barbulovski <bbarbulovski@gmail.com>
2013-10-15 04:46:57 +0000

    *deb support(for ubuntu's launchpad) * remove old build/archive zip files

--- backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-chmod 744 *.sh
-
-DATE=`date +%Y%m%d%H%M%S`
-
-# TODO: please set zip command not to delete empty dirs(ex: bin)
-
-zip backup-mkfusion-0.5.0_$DATE.zip * -r -x "*.dll" -x "obj/*" -x "WINDOWS/*.exe" -x "bin/*.exe" -x "*.zip" -x ".svn"
