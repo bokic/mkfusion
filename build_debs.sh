040d780a65988ee435e2d69f87f850bce1719fbe
Boris Barbulovski <bbarbulovski@gmail.com>
2013-10-15 06:58:10 +0000

    *deb support(for ubuntu's launchpad) WIP

---
+++ build_debs.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+rm -f ../mkfusion_0.5.0*
+rm -f ../cfeditor_0.5.0*
+rm -f ../libapache2-mod-mkfusion_0.5.0*
+
+rm -f bin/mkfusion
+rm -f bin/libmod_mkfusion*
+
+dh_make -p mkfusion_0.5.0 -m -y -d -c lgpl3 --createorig || debuild
+
+rm -f debian/files
+
+rm -f debian/mkfusion.debhelper.log
+rm -f debian/cfeditor.debhelper.log
+rm -f debian/libapache2-mod-mkfusion.debhelper.log
+
+rm -f debian/mkfusion.substvars
+rm -f debian/cfeditor.substvars
+rm -f debian/libapache2-mod-mkfusion.substvars
+
+rm -fr debian/mkfusion
+rm -fr debian/cfeditor
+rm -fr debian/libapache2-mod-mkfusion
