b3bf2fd9955957ae92055548a1907014f11bc91c
Boris Barbulovski <bbarbulovski@gmail.com>
2013-11-02 04:07:41 +0000

    mkfusion daemon re-architecture(wip)(add missing qcfapplication.cpp)

---
+++ src/mkfusion/appserver/qcfapplication.cpp
@@ -0,0 +1,56 @@
+#include "qcfapplication.h"
+
+QCFApplication::QCFApplication()
+    : m_sessionManagement(false)
+    , m_setClientCookies(false)
+    , m_callOnApplicationEnd(false)
+{
+}
+
+QCFApplication::QCFApplication(const QCFApplication &other)
+    : m_name(other.m_name)
+    , m_path(other.m_path)
+    , m_applicationTemplate(other.m_applicationTemplate)
+    , m_sessionManagement(other.m_sessionManagement)
+    , m_setClientCookies(other.m_setClientCookies)
+    , m_callOnApplicationEnd(other.m_callOnApplicationEnd)
+    , m_data(other.m_data)
+    //, m_mutexHash(other.m_mutexHash)
+    , m_timeout(other.m_timeout)
+    //, m_mutex(other.m_mutex)
+{
+}
+
+#ifdef Q_COMPILER_RVALUE_REFS
+QCFApplication &QCFApplication::operator=(QCFApplication &&other)
+{
+    qSwap(m_name, other.m_name);
+    qSwap(m_path, other.m_path);
+    qSwap(m_applicationTemplate, other.m_applicationTemplate);
+    qSwap(m_sessionManagement, other.m_sessionManagement);
+    qSwap(m_setClientCookies, other.m_setClientCookies);
+    qSwap(m_callOnApplicationEnd, other.m_callOnApplicationEnd);
+    qSwap(m_data, other.m_data);
+    //qSwap(m_mutexHash, other.m_mutexHash);
+    qSwap(m_timeout, other.m_timeout);
+    //qSwap(m_mutex, other.m_mutex);
+
+    return *this;
+}
+#endif
+
+QCFApplication &QCFApplication::operator=(const QCFApplication &other)
+{
+    m_name = other.m_name;
+    m_path = other.m_path;
+    m_applicationTemplate = other.m_applicationTemplate;
+    m_sessionManagement = other.m_sessionManagement;
+    m_setClientCookies = other.m_setClientCookies;
+    m_callOnApplicationEnd = other.m_callOnApplicationEnd;
+    m_data = other.m_data;
+    //m_mutexHash = other.m_mutexHash;
+    m_timeout = other.m_timeout;
+    //m_mutex = other.m_mutex
+
+    return *this;
+}
