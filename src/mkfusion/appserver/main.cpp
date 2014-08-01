2d21c11ae6c28223362d6b8b855667e170154bbd
Boris Barbulovski <bbarbulovski@gmail.com>
2014-08-01 21:43:14 +0200

    Create QCoreApplication for precompiled headers.
    Signed-off-by: Boris Barbulovski <bbarbulovski@gmail.com>

--- src/mkfusion/appserver/main.cpp
+++ src/mkfusion/appserver/main.cpp
@@ -8,6 +8,8 @@ int main(int argc, char **argv)
 {
     if ((argc == 2)&&(strcmp(argv[1], "-precompile") == 0))
     {
+        QCoreApplication app(argc, argv);
+
         QDir path = QDir(QCoreApplication::applicationDirPath());
         path.cdUp();
 
