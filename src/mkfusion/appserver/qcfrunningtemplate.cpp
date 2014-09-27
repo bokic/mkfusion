f55b690d46725b2c6eb4121b85ed24935aa8d1f7
Boris Barbulovski <bbarbulovski@gmail.com>
2014-09-27 13:33:38 +0200

    Put git build string into COLDFUSION.PRODUCTVERSION

--- src/mkfusion/appserver/qcfrunningtemplate.cpp
+++ src/mkfusion/appserver/qcfrunningtemplate.cpp
@@ -16,6 +16,11 @@
 #include <QDir>
 #include <QUrl>
 
+#ifndef BUILDNUMBER
+#define BUILDNUMBER "unknown"
+#endif
+
+const char *build = BUILDNUMBER;
 
 QCFRunningTemplate::QCFRunningTemplate()
     : QObject()
@@ -541,7 +546,7 @@ void QCFRunningTemplate::worker()
 #endif
                     cf_StructUpdate(m_SERVER[QStringLiteral("COLDFUSION")], QStringLiteral("PRODUCTLEVEL"), QStringLiteral("Free"));
                     cf_StructUpdate(m_SERVER[QStringLiteral("COLDFUSION")], QStringLiteral("PRODUCTNAME"), QStringLiteral("MKFusion Server"));
-                    cf_StructUpdate(m_SERVER[QStringLiteral("COLDFUSION")], QStringLiteral("PRODUCTVERSION"), QStringLiteral("0.5.0"));
+                    cf_StructUpdate(m_SERVER[QStringLiteral("COLDFUSION")], QStringLiteral("PRODUCTVERSION"), QStringLiteral(BUILDNUMBER));
                     cf_StructUpdate(m_SERVER[QStringLiteral("COLDFUSION")], QStringLiteral("ROOTDIR"), m_CFServer->m_MKFusionPath.left(-1));
                     cf_StructUpdate(m_SERVER[QStringLiteral("COLDFUSION")], QStringLiteral("SUPPORTEDLOCALES"), QStringLiteral("English (US),en,en_US"));
                     cf_StructUpdate(m_SERVER, QStringLiteral("OS"), QWDDX(QWDDX::Struct));
