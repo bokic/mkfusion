9488450208578ebf269d6f88cc6c824116b401b1
Boris Barbulovski <bbarbulovski@gmail.com>
2015-01-17 11:59:43 +0100

    Set ParserTest1 project to use definitions from src dir.

--- src/tests/parsertest1/main.cpp
+++ src/tests/parsertest1/main.cpp
@@ -1,9 +1,13 @@
 #include <QApplication>
 #include "parsertest1.h"
+#include "qtextparser.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QTextParser::loadParserDefinitionsFromDir("../src/textparser");
+
     CFTest1 mainWindow;
     mainWindow.show();
     return a.exec();
